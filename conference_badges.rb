7ca86f044a6aca70beab5bf547ed63997ffe5d2e
Jorge Venegas <brangelina01@gmail.com>
2020-07-14 04:53:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ def batch_badge_creator(attendees)
   end
 end
 
-def assign_rooms(attendees)
+def room_assignments(attendees)
     array_x = [] 
     attendees.each_with_index do |name, room|
     array_x << "Hello, #{name}! You’ll be assigned to room #{room + 1}!"
